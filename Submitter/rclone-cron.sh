f03239189aca3d666b752e21972b1bf83528c7ff
Bogdan Ioan BRUDIU <bogdanbrudiu@gmail.com>
2019-01-04 17:16:54 +0200

    git-secret added

--- Submitter/rclone-cron.sh
+++ Submitter/rclone-cron.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-[ "${FLOCKER}" != "$0" ] && exec env FLOCKER="$0" flock -en "$0" "$0" "$@" || echo $(date -u)' | rclone-cron.sh already running.' >> $CRONLOG 
+
 
 #### rclone sync options
 
@@ -15,13 +15,30 @@ LOGS='-vv --log-file='$LOGFILE
 #---- Location of cron log
 CRONLOG=$SRC/.rclone/rclone-cron.log
 
+# Better error handling. Log which script failed and where.
+die()
+{
+  echo "${datestamp} error in \"$0\" at line $BASH_LINENO" \
+      >> ${CRONLOG}
+  exit 1
+}
+
+# Log all STDERR
+exec 2>> ${CRONLOG}
+
+trap die ERR SIGTERM SIGINT
+
 #Rotate logs.
 savelog -n -c 7 $LOGFILE >> $CRONLOG
+
+[ "${FLOCKER}" != "$0" ] && exec env FLOCKER="$0" flock -en "$0" "$0" "$@" || echo $(date -u)' | rclone-cron.sh already running.' >> $CRONLOG 
+
 
 #Log startup
 echo $(date -u)' | starting rclone-cron.sh . . .' >> $CRONLOG
 
 #Now do the sync!
+echo $(date -u)' | rclone sync '$SRC' '$DEST' --transfers=2 --checkers=4 --min-age=1m --exclude-from '$SRC'/.rclone/.rclone-ignore  --exclude-if-present .rclone-ignore --delete-excluded '$LOGS >> $CRONLOG
 rclone sync $SRC $DEST --transfers=2 --checkers=4 --min-age=1m --exclude-from $SRC/.rclone/.rclone-ignore  --exclude-if-present .rclone-ignore --delete-excluded $LOGS
 
 #log success
